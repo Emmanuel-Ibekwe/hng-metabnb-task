b815df49475b7acd6f4c22e566699cc3126fb5a6
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-11 13:04:18 +0100

    chore: Add component parts of the TopNavigation component

--- src/components/MainHeader/TopNavigation.js
+++ src/components/MainHeader/TopNavigation.js
@@ -1,5 +1,33 @@
+import houseLogo from "../../assets/house.png";
+import classes from "./TopNavigation.module.css";
+
 const TopNavigation = () => {
-  return <nav></nav>;
+  return (
+    <nav className={classes["top-nav"]}>
+      <div className={classes["logo__box"]}>
+        <div className="logo__img">
+          <img src={houseLogo} alt="logo image" />
+        </div>
+        <h1>Metabnb</h1>
+      </div>
+      <ul>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Place to stay</a>
+        </li>
+        <li>
+          <a>NFTs</a>
+        </li>
+        <li>
+          <a>Community</a>
+        </li>
+      </ul>
+
+      <button>Connect wallet</button>
+    </nav>
+  );
 };
 
 export default TopNavigation;
