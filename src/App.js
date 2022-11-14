05e1ae838a14ac85cb35208ff4dd1d11bbcf9b80
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-14 05:57:42 +0100

    chore: Update the file path of TopNavigation in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import MainContent from "./components/MainContent/MainContent";
-import TopNavigation from "./components/MainHeader/TopNavigation";
+import TopNavigation from "./components/TopNavigation/TopNavigation";
 import Footer from "./components/Footer/Footer";
 import Layout from "./components/Layout/Layout";
 
