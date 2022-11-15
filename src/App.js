63e03bf560b27e5d61d8281ff227a210618f37d5
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-15 12:14:07 +0100

    style: Add a few styliing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Home from "./pages/Home";
+import PlaceToStay from "./pages/PlaceToStay";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <PlaceToStay />
     </div>
   );
 }
