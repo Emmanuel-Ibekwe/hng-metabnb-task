44d92fc48e8e80968899299aee4436e3379f2a96
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-15 11:04:05 +0100

    refactor: Include Footer and Topnavigation into the Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,15 @@
+import TopNavigation from "./TopNavigation/TopNavigation";
 import styles from "./Layout.module.css";
+import Footer from "./Footer/Footer";
 
 const Layout = (props) => {
-  return <div className={styles.layout}>{props.children}</div>;
+  return (
+    <div className={styles.layout}>
+      <TopNavigation />
+      {props.children}
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
---
+++ src/components/Layout/TopNavigation/TopNavigation.js
@@ -0,0 +1,45 @@
+import { Fragment } from "react";
+import houseLogo from "../../assets/house.png";
+import ConnectWalletModal from "../../ConnectWalletModal/ConnectWalletModal";
+import MenuIcon from "../../Icons/SocialIcons/MenuIcon";
+import classes from "./TopNavigation.module.css";
+
+const TopNavigation = () => {
+  return (
+    <Fragment>
+      <nav className={classes["top-nav"]}>
+        <div className={classes["logo__box"]}>
+          <div className="logo__img">
+            <img src={houseLogo} alt="logo" />
+          </div>
+          <h1>Metabnb</h1>
+        </div>
+        <ul>
+          <li className={classes["close-btn"]}>
+            <span>&times;</span>
+          </li>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Place to stay</a>
+          </li>
+          <li>
+            <a>NFTs</a>
+          </li>
+          <li>
+            <a>Community</a>
+          </li>
+        </ul>
+
+        <div className={classes.actions}>
+          <button>Connect wallet</button>
+          <MenuIcon className={classes.menu} />
+        </div>
+      </nav>
+      <ConnectWalletModal />
+    </Fragment>
+  );
+};
+
+export default TopNavigation;
