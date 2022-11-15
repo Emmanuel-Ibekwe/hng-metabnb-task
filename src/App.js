f190e1d8e1e205bc487165a4c8fee34c239f2adf
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-15 12:37:19 +0100

    feat: Add Routing to the application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
+import TopNavigation from "./components/Layout/TopNavigation/TopNavigation";
 import Home from "./pages/Home";
 import PlaceToStay from "./pages/PlaceToStay";
 
 function App() {
   return (
     <div className="App">
-      <PlaceToStay />
+      <TopNavigation />
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/place-to-stay" element={<PlaceToStay />} />
+      </Routes>
     </div>
   );
 }
