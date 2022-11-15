ba0d13353604830e3382af3f31a7c6341178b708
Emmanuel-Ibekwe <emmanuelibekwe7@gmail.com>
2022-11-15 12:12:42 +0100

    feat: Create PlaceToStay page

---
+++ src/pages/PlaceToStay.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout/Layout";
+import NFTCardSection from "../components/NFTCard/NFTCardSection";
+import SubNavigation from "../components/SubNavigation/SubNavigation";
+
+const PlaceToStay = () => {
+  return (
+    <Layout>
+      <SubNavigation />
+      <NFTCardSection />
+    </Layout>
+  );
+};
+
+export default PlaceToStay;
